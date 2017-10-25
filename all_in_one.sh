ff549867c7ca1229bc17d7ac418ed67fe48781ef
Xiyang Dai <vdavid70619@users.noreply.github.com>
2017-10-24 23:36:04 -0400

    Update all_in_one.sh

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,14 +1,14 @@
 
 # define home dir, should contain tsn_score, mbh, shuffle_imagenet features path
-export PYTHONPATH=$PYTHONPATH:/home/xiyang/mycaffe_new/python
-export ACTNET_HOME=/home/DATASETS/actnet
+export PYTHONPATH=$PYTHONPATH:path
+export ACTNET_HOME=path
 
-#python -u generate_proposal.py
+python -u generate_proposal.py
 
 python -u train_proposal_pair2.py
 python -u train_proposal_class2.py
 
-#python -u train_chunk_mbh.py
-#python -u train_chunk_imshuffle.py
+python -u train_chunk_mbh.py
+python -u train_chunk_imshuffle.py
 
-#python -u evaluate.py
+python -u evaluate.py
